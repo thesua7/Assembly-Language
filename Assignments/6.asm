c2b16135a96f9e6904b100247c7dafda199805ae
Sani Ahamed <thesua711@gmail.com>
2020-04-07 02:39:03 +0600

    More Workz

---
+++ Assignments/6.asm
@@ -0,0 +1,46 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+ 
+           
+mov ax, 000000000000000b
+mov ds, ax   
+mov si, 000000000000000b    
+MOV byte ptr ds:[si],3h
+MOV bl, byte ptr ds:[si]  
+ 
+ 
+           
+mov ax, 1111000000000000b
+mov ds, ax   
+mov si, 1111111111111111b    
+MOV byte ptr ds:[si],2h
+ 
+           
+MOV bh, byte ptr ds:[si]
+
+
+add bh,bl    
+cmp bh,5h
+jg Store 
+jmp Exit 
+
+Store:
+mov ax, 1111000000000000b
+mov ds, ax   
+mov si, 1111111111111110b    
+MOV byte ptr ds:[si],bh
+jmp Exit
+
+
+Exit:
+end
+
+
+ret
+
+
+
+
